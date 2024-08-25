723f994fde7caf9e4ee11745b4cb8c33359ddaef
lukeisun <delacruz@adinlukas.xyz>
2024-08-24 20:02:13 -0500

    server responds with hello (works in browser!)

--- src/main.zig
+++ src/main.zig
@@ -14,6 +14,7 @@ pub fn get_op(data: u64) UserData {
     const op: u8 = @truncate(data >> 56);
     return @enumFromInt(op);
 }
+
 pub fn main() !void {
     const socket = try posix.socket(posix.AF.INET, posix.SOCK.STREAM, 0);
     defer posix.close(socket);
@@ -34,11 +35,18 @@ pub fn main() !void {
     defer io.deinit();
     try posix.listen(socket, 128);
     const buf_size = 128;
-    const buf_count = 2;
+    const buf_count = 10;
     var buffers: [buf_count][buf_size]u8 = undefined;
     _ = try io.provide_buffers(fmt_data(UserData.PROVIDE_BUFFERS), @as([*]u8, @ptrCast(&buffers)), buf_size, buf_count, 0, 0);
     _ = try io.accept(fmt_data(UserData.ACCEPT), socket, &client_addr.any, &client_size, 0);
     _ = try io.submit();
+    const recv_buf = io_t.RecvBuffer{
+        .buffer_selection = .{
+            .group_id = 0,
+            .len = buf_size,
+        },
+    };
+
     // user_data
     // ................................................................
     // ^^^^^^^^                                        ^^^^^^^^^^^^^^^^
@@ -46,6 +54,22 @@ pub fn main() !void {
     while (true) {
         while (io.cq_ready() > 0) {
             const cqe = try io.copy_cqe();
+            switch (cqe.err()) {
+                .SUCCESS => {},
+                .NOBUFS => {
+                    buffers = undefined;
+                    _ = try io.provide_buffers(fmt_data(UserData.PROVIDE_BUFFERS), @as([*]u8, @ptrCast(&buffers)), buf_size, buf_count, 0, 0);
+                    // Just replay the event
+                    if (get_op(cqe.user_data) == .RECV) {
+                        const fd: u16 = @truncate(cqe.user_data);
+                        recv(&io, cqe.user_data, fd, recv_buf, 0);
+                    }
+                    _ = try io.submit_and_wait(2);
+                    continue;
+                },
+                else => unreachable,
+            }
+            print("{s}\n", .{@tagName(get_op(cqe.user_data))});
             const op: UserData = get_op(cqe.user_data);
             switch (op) {
                 .ACCEPT => {
@@ -55,43 +79,26 @@ pub fn main() !void {
                     const new_fd: u16 = @intCast(cqe.res);
                     const user_data: u64 = fmt_data(UserData.RECV) | new_fd;
                     std.log.info("{any} {d}\n", .{ client_addr, new_fd });
-                    _ = try io.recv(
-                        user_data,
-                        new_fd,
-                        io_t.RecvBuffer{
-                            .buffer_selection = .{
-                                .group_id = 0,
-                                .len = buf_size,
-                            },
-                        },
-                        0,
-                    );
+                    recv(&io, user_data, new_fd, recv_buf, 0);
+                    _ = try io.accept(fmt_data(UserData.ACCEPT), socket, &client_addr.any, &client_size, 0);
                 },
                 .RECV => {
                     const buf_id = try cqe.buffer_id();
                     // process
-
+                    const buf = "HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello";
                     //send back
+                    print("{s}\n", .{buffers[buf_id]});
                     const fd: u16 = @truncate(cqe.user_data);
-                    _ = try io.send(fmt_data(UserData.SEND), fd, &buffers[buf_id], 0);
-                    _ = try io.recv(
-                        cqe.user_data,
-                        fd,
-                        io_t.RecvBuffer{
-                            .buffer_selection = .{
-                                .group_id = 0,
-                                .len = buf_size,
-                            },
-                        },
-                        0,
-                    );
+                    _ = try io.send(fmt_data(UserData.SEND), fd, buf, 0);
                 },
                 .PROVIDE_BUFFERS => {},
                 .SEND => {},
                 else => unreachable,
             }
         }
-
         _ = try io.submit();
     }
+}
+fn recv(iou: *io_t, data: u64, fd: i32, recv_buf: io_t.RecvBuffer, flags: u8) void {
+    _ = iou.recv(data, fd, recv_buf, flags) catch unreachable;
 }
